c4b846871c168c433690a933f4c2ca22d99783f8
TuğçeTaş <tugcetastas.3@gmail.com>
2023-11-18 17:11:16 +0300

    first commit

---
+++ Triangle/TriangleStars.cs
@@ -0,0 +1,25 @@
+﻿namespace Triangle;
+
+public class TriangleStars
+{
+    public void Stars() {
+    
+        Console.Write("Sayı girin: ");
+        int n = Convert.ToInt16(Console.ReadLine());
+    
+        for (int i=1; i<=n ; i++) 
+        { 
+            for (int j=1; j<=(n-i) ; j++) 
+            {
+                Console.Write(" ");
+            }
+            for (int k=1; k<=(2*i-1) ; k++) 
+            {
+                Console.Write("*");
+            }
+        
+            Console.WriteLine();
+        }
+    }
+}
+
